0f0ed8fae3b23f47e1f8bf163049b2bb17c0d407
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-02 23:25:27 +0000

    Add solution to task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,25 @@ class Auth:
         Returns:
                 bool: _description_
         """
-        return False
+        if path is None:
+            return True
+        
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+        
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """_summary_
