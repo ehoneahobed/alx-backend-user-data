e04e09d002466904e371b379dbc6896f57b671d2
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-03 00:44:56 +0000

    Add solution to task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,8 +4,11 @@ Module for authentication using Basic auth
 """
 
 
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 import base64
+
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -17,10 +20,10 @@ class BasicAuth(Auth):
         """_summary_
 
         Args:
-                        authorization_header (str): _description_
+                                        authorization_header (str): _description_
 
         Returns:
-                        str: _description_
+                                        str: _description_
         """
         if authorization_header is None:
             return None
@@ -37,10 +40,10 @@ class BasicAuth(Auth):
         """_summary_
 
         Args:
-                        base64_authorization_header (str): _description_
+                                        base64_authorization_header (str): _description_
 
         Returns:
-                        str: _description_
+                                        str: _description_
         """
         if base64_authorization_header is None:
             return None
@@ -59,8 +62,8 @@ class BasicAuth(Auth):
         """_summary_
 
         Args:
-                self (_type_): _description_
-                str (_type_): _description_
+                        self (_type_): _description_
+                        str (_type_): _description_
         """
         if decoded_base64_authorization_header is None:
             return (None, None)
@@ -71,3 +74,26 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':')
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """_summary_
+
+        Args:
+                self (_type_): _description_
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
