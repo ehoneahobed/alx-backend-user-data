49dfb19876e931514f30ac7d03fc731d597a000f
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-03 00:11:25 +0000

    Fix: Update for pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,26 +8,25 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-	"""_summary_
-	"""
+    """_summary_
+    """
 
-	def extract_base64_authorization_header(self, authorization_header: str) -> str:
-		"""_summary_
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """_summary_
 
-		Args:
-				authorization_header (str): _description_
+        Args:
+                        authorization_header (str): _description_
 
-		Returns:
-				str: _description_
-		"""
-		if authorization_header is None:
-			return None
-		if not isinstance(authorization_header, str):
-			return None
-		if not authorization_header.startswith('Basic '):
-			return None
+        Returns:
+                        str: _description_
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
 
-		token = authorization_header.split(' ')[-1]
-		return token
-
-  
+        token = authorization_header.split(' ')[-1]
+        return token
