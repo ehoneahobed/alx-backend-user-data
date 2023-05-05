9e1827e010602bc9755f15eb55406f30ddc82927
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-05 00:19:27 +0000

    Add solution to task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,10 +19,10 @@ class SessionAuth(Auth):
         """_summary_
 
         Args:
-                user_id (str, optional): _description_. Defaults to None.
+            user_id (str, optional): _description_. Defaults to None.
 
         Returns:
-                str: _description_
+            str: _description_
         """
         if user_id is None or not isinstance(user_id, str):
             return None
@@ -30,3 +30,16 @@ class SessionAuth(Auth):
         id = uuid4()
         self.user_id_by_session_id[str(id)] = user_id
         return str(id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """_summary_
+
+        Args:
+            session_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+                str: _description_
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
