48df7c659c929c39acc0d274cdf31b73058633d1
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-05 01:10:53 +0000

    Add solution to task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -38,7 +38,7 @@ class SessionAuth(Auth):
             session_id (str, optional): _description_. Defaults to None.
 
         Returns:
-            str: _description_
+                str: _description_
         """
         if session_id is None or not isinstance(session_id, str):
             return None
@@ -54,3 +54,23 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_cookie)
         user = User.get(user_id)
         return user
+
+    def destroy_session(self, request=None):
+        """_summary_
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -29,3 +29,12 @@ def session_auth():
             resp.set_cookie(session_name, session_id)
             return resp
     return jsonify({"error": "wrong password"}), 401
+
+def handle_logout():
+    """
+    for logging out user
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
