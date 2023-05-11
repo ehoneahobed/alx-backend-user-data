5d39975513c3829498297f86baa34fba999d7ec4
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-11 12:52:19 +0000

    Add solution to task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,10 @@
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -13,6 +16,21 @@ def index() -> str:
     """_summary_
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """_summary_
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # regsiter user if user does not exist
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
