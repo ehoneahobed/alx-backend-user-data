a35d975a74831ed5b6b57d8f4ba66f828e2c5bfa
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-11 01:47:29 +0000

    Add solution to task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """_summary_
 
         Args:
