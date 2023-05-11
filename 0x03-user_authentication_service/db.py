5b38929b6e23b75d14bc6e3c7b7773602a56486c
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-11 01:43:55 +0000

    Add solution to task 1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,46 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password) -> User:
+        """_summary_
+
+        Args:
+            email (_type_): _description_
+            hashed_password (bool): _description_
+
+        Returns:
+            User: _description_
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        # add new user and commit to database
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
