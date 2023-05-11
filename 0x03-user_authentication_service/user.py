d228bcb5a506d51386f0125db55ec0bf6181efa3
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-11 02:12:56 +0000

    Update solution to task 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -19,5 +19,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
