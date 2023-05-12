bfbe09e67d8423bd9e4c8b070a518b9990cef115
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-12 00:07:01 +0000

    Add solution to task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,3 +109,17 @@ class Auth:
             return None
         else:
             return user
+
+    def destroy_session(self, user_id: str) -> None:
+        """_summary_
+
+        Args:
+            user_id (str): _description_
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = None
+            return None
