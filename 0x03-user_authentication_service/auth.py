f115f302b283247707beca788ab8fdaacee2df40
ehoneahobed <ehoneahobed@hotmail.com>
2023-05-12 00:26:26 +0000

    Add solution to task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -123,3 +123,20 @@ class Auth:
         else:
             user.session_id = None
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """_summary_
+
+        Args:
+            email (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.reset_token = _generate_uuid()
+            return user.reset_token
